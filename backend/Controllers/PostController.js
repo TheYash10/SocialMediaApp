e2b1dca0eaf2850e85f7af91698f2b6aa7cb631e
TheYash10 <yashjariwala121@gmail.com>
2024-01-05 17:44:49 +0530

    https://github.com/TheYash10/SocialMediaWebApp.git

---
+++ backend/Controllers/PostController.js
@@ -0,0 +1,126 @@
+import PostModel from "../Models/postModel.js";
+import mongoose from "mongoose";
+
+import UserModel from "../Models/userModel.js";
+
+// Create new Post
+export const createPost = async (req, res) => {
+  const newPost = new PostModel(req.body);
+
+  try {
+    await newPost.save();
+    res.status(200).json("Post created!");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Create a post
+export const getPost = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const post = await PostModel.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Update a post
+export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post Updated");
+    } else {
+      res.status(403).json("action forbidden");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Delete a post
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(id);
+
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json("Post Deleted Successfully");
+    } else {
+      res.status(403).json("action forbidden");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// like/disLike a post
+
+export const likePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(id);
+    if (!post.likes.includes(userId)) {
+      await post.updateOne({ $push: { likes: userId } });
+      res.status(200).json("Post Liked");
+    } else {
+      await post.updateOne({ $pull: { likes: userId } });
+      res.status(200).json("Post unliked");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Get Timeline Posts:- Timeline posts include his/her own posts and post of the person he/she following
+export const getTimelinePosts = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const currentUserPosts = await PostModel.find({ userId: userId });
+    const followingPosts = await UserModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "following",
+          foreignField: "userId",
+          as: "followingPosts",
+        },
+      },
+      {
+        // project field is the return type of aggregation in which we just show which field is required and which field is not required
+        $project: {
+          followingPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json(
+      currentUserPosts
+        .concat(...followingPosts[0].followingPosts)
+        .sort((a, b) => {
+          return b.createdAt - a.createAt;
+        })
+    );
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser"; //middleware
+import mongoose from "mongoose"; //to create the model and schema for our mongodb
+import dotenv from "dotenv";
+import cors from "cors";
+import AuthRoute from "./Routes/AuthRoute.js";
+import UserRoute from "./Routes/UserRoute.js";
+import PostRoute from "./Routes/PostRoute.js";
+
+// Routes
+const app = express();
+
+// Middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error));
+
+// Usage of routes
+app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
+app.use("/post", PostRoute);
